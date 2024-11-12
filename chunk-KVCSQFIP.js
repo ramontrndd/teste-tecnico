df310822b6f408c05be042f86b8eafa15449d941
ramontrndd <134494678+ramontrndd@users.noreply.github.com>
2024-11-12 16:48:03 +0000

    Deploying to prod from @ ramontrndd/teste-tecnico@7018225301b877fa0aeac97ad229950accf97652 🚀

--- chunk-KVCSQFIP.js
+++
@@ -1,1 +0,0 @@
-import{f as a}from"./chunk-RW3N5VI2.js";import"./chunk-5GKAZTVB.js";export{a as AppComponent};
---
+++ chunk-UNG2LPS2.js
@@ -0,0 +1,1 @@
+import{f as a}from"./chunk-N4WSUWAS.js";import"./chunk-R63GXJBB.js";export{a as AppComponent};
